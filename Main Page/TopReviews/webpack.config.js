9da0232d7e65dc290f61fdbd2885acb6d2c35657
noursaqqa <noursaqqa7@gmail.com>
2019-11-18 11:49:53 +0200

    webpack ready for the first three modules

--- Main Page/TopReviews/webpack.config.js
+++ Main Page/TopReviews/webpack.config.js
@@ -1,21 +1,24 @@
-module.exports = {
-  entry: __dirname + '/client/src/index.jsx',
+var path = require('path');
+var SRC_DIR = path.join(__dirname, './client/src');
+var DIST_DIR = path.join(__dirname, './client/src/dist');
+
+module.exports =[
+  { 
+  mode: 'development',
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
   module: {
-    rules: [
+    loaders : [
       {
-        test: [/\.jsx$/],
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env']
-          }
+        test : /\.jsx?/,
+        include : SRC_DIR, 
+        loader : 'babel-loader',      
+        query: {
+          presets: ['react', 'es2015']
         }
       }
     ]
-  },
-   output: {
-    filename: 'bundle.js',
-    path: __dirname + '/client/dist'
-  }
-};
+  }}];
