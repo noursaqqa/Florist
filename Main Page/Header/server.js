2bb8b98a91f7e6573f443790f648d9a49de72d7b
unknown <hadil.a.hassouna@gmail.com>
2019-11-19 05:19:03 -0800

    log in page

--- Main Page/Header/server.js
+++ Main Page/Header/server.js
@@ -2,16 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const request = require('request');
-var app = express()
-var db = require('./db.js');
 
-app.use(express.static(__dirname + '/'));
+var app = express()
+// var db = require('./db.js');
+
+
+
+
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//  console.log(" we're connected");
+// });
+
+app.use(express.static(__dirname + '/client/src/dist'));
+// app.use(express.static(__dirname + '/client/src/components'));
+
+// const App = require("./db.js").App
 
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(bodyParser.json()); 
 
 
-app.get ('/', (req, res) => {
+// app.get ('/processFromData', (req, res) => {
+//   res.send('Hello from Header server');
+// }); 
+app.get ('/app', (req, res) => {
+
   res.send('Hello from Header server');
 });
 
