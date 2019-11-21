76dfa0d301aa6f96de03af3ad4b2c5028a3581d7
abudiab <mahmoud.w.diab@gmail.com>
2019-11-21 15:47:05 +0200

    webpack donfig with jest

--- MainPage/Mainbody/server.js
+++ MainPage/Mainbody/server.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const request = require('request');
 var app = express()
-var db = require('./database/data.js.js');
-var db = require('./database/data.js.js');
+var db = require('./database/data.js');
 
 app.use(express.static('./client/src/dist'));
 
--- MainPage/Mainbody/webpack.config.js
+++ MainPage/Mainbody/webpack.config.js
@@ -20,5 +20,24 @@ module.exports =[
           presets: ["@babel/preset-env", "@babel/preset-react"]
         }
       }
-    ]
+    ],
+    loaders: [
+      {exclude: ['node_modules'], loader: 'babel', test: /\.jsx?$/},
+      {loader: 'style-loader!css-loader', test: /\.css$/},
+      {loader: 'url-loader', test: /\.gif$/},
+      {loader: 'file-loader', test: /\.(ttf|eot|svg)$/},
+    ],
+  },
+  resolve: {
+    alias: {
+      config$: './configs/app-config.js',
+      react: './vendor/react-master',
+    },
+    extensions: ['', 'js', 'jsx'],
+    modules: [
+      'node_modules',
+      'bower_components',
+      'shared',
+      '/shared/vendor/modules',
+    ],
   }}];
