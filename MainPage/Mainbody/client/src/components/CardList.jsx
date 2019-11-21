9ad8f3f9da0cf8b8d378db2a926f248b1197a493
Basima1994 <beso10111994@gmail.com>
2019-11-21 15:21:19 +0200

    id fixed

--- MainPage/Mainbody/client/src/components/CardList.jsx
+++ MainPage/Mainbody/client/src/components/CardList.jsx
@@ -1,39 +1,29 @@
 //class
-import React from 'react'
-import CardItem from './CardItem.jsx'
-
+import React from "react";
+import CardItem from "./CardItem.jsx";
 
 class CardList extends React.Component {
-    constructor(props) {
-        super(props);
-        // this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    // this.state = {};
+  }
 
-    render() {
-        // console.log('from cardlist render', this.props);
-        // var cards = [];
-        // if(this.props.cards.length !== 0) {
-        //     cards = this.props.cards;
-        // }
+  render() {
+    // console.log('from cardlist render', this.props);
+    // var cards = [];
+    // if(this.props.cards.length !== 0) {
+    //     cards = this.props.cards;
+    // }
 
-        // console.log('from cardlist render**',this.props.cards);
-        return (
-
-            <div className="main-container">             
-                {this.props.cards.map(card => {
-                    return <CardItem card={card}/>
-               })}
-                
-                
-            </div>
-
-  
-            
-        );
-    }
+    // console.log('from cardlist render**',this.props.cards);
+    return (
+      <div className="main-container">
+        {this.props.cards.map(card => {
+          return <CardItem card={card} />;
+        })}
+      </div>
+    );
+  }
 }
-
-
-
 
 export default CardList;
