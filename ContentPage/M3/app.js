ff5ebe4bd7a4467439cca272c185d0a3c63dfae1
abudiab <mahmoud.w.diab@gmail.com>
2019-11-23 00:51:10 +0200

    testing for M1 and M2

---
+++ ContentPage/M3/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+let app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+app.use(express.static("client/dist"));
+app.use(bodyParser.json({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static("../public"));
+
+var db = require("./data.js");
+
+app.get("/items", (req, res) => {
+  console.log("i'm inside the get");
+  db.Flower.find({})
+    .sort({ date: -1 })
+    .then(items => res.json(items), console.log("items inside the.Get server"));
+  // .then(items => res.json(items));
+});
+
+app.get("/id", function(req, res) {
+  var n = req.query.id;
+  console.log(n);
+
+  db.Flower.findOne({_id: n}, (err, data) => {
+    if (err) {
+      console.log("error", err);
+    }
+    console.log(data);
+    res.status(200).send(data);
+  });
+});
+
+module.exports = app;
